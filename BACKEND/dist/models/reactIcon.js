3a49be7f4c82a3e714df653695955ac078f98597
RaphVSSR <Raphael.vasseur1@outlook.fr>
2026-04-04 15:48:57 +0200

    Add dist/ to gitignore, remove tracked build artifacts

--- BACKEND/dist/models/reactIcon.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const Schema = mongoose.Schema;
-const reactIconSchema = new Schema({
-    react_icon_uuid: { type: String, required: true },
-    react_icon_name: { type: String, required: true },
-    react_icon_url: { type: String, required: true },
-});
-export default mongoose.model("ReactIcon", reactIconSchema);
-//# sourceMappingURL=reactIcon.js.map
