0ac30229860345ddbd39020252bda00f1012b0ef
La-Tva <144448452+La-Tva@users.noreply.github.com>
2026-05-03 23:29:26 +0200

    fix: register update_user messages in controller abstracts

--- BACKEND/src/Controller/Controller.abstracts.ts
+++ BACKEND/src/Controller/Controller.abstracts.ts
@@ -18,9 +18,10 @@ export function init() {
 		["auth_success", "auth_failure", "login_success", "login_failure", "login_pending",
 			"registration_success", "registration_failure", "user_disconnect_success",
 			"session_refreshed", "session_expired",
-			"session_pending", "session_pending_accepted", "session_pending_rejected"],
+			"session_pending", "session_pending_accepted", "session_pending_rejected",
+			"update_user_response"],
 		["authenticate", "login", "register", "user_disconnect", "session_refresh",
-			"session_pending_choice", "client_deconnexion"]
+			"session_pending_choice", "client_deconnexion", "update_user_request"]
 	)
 
 	new DirectoryService(controleur, SocketIO.server, "DirectoryService")
