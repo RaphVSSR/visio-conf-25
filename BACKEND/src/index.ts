55ac4ffaa802200c480c08e286def865e840927b
SiulMigouel <luismiguel.meiraribeiro@gmail.com>
2026-05-05 17:06:08 +0200

    Adapt permissions deployment config

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -51,7 +51,13 @@ try {
 
 	const socketServer = new Server(HTTPServer.server, {
 		cors: {
-			origin: ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001"],
+			origin: (origin, callback) => {
+				if (RestService.isOriginAllowed(origin)) {
+					callback(null, true)
+				} else {
+					callback(new Error("Not allowed by CORS"))
+				}
+			},
 			methods: ["GET", "POST"],
 			credentials: true,
 		}
