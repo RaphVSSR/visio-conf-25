425695541624a2e2d4364d9528408f2038888d9c
SuperAkiCodeur <jesuisakashkulbir@gmail.com>
2026-05-07 10:09:55 +0200

    fix: rest service

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -51,19 +51,17 @@ try {
 	const expressApp = await RestService.implement()
 	HTTPServer.createFromExpress(expressApp)
 
-    const socketServer = new Server(HTTPServer.server, {
-        cors: {
-            origin: (origin, callback) => {
-                if (RestService.isOriginAllowed(origin)) {
-                    callback(null, true)
-                } else {
-                    callback(new Error("Not allowed by CORS"))
-                }
-            },
-            methods: ["GET", "POST"],
-            credentials: true,
-        }
-    })
+	const socketServer = new Server(HTTPServer.server, {
+		cors: {
+			origin: (origin, callback) => {
+				if (RestService.isOriginAllowed(origin)) callback(null, true)
+				else callback(new Error("Not allowed by CORS"))
+			},
+			methods: ["GET", "POST"],
+			credentials: true,
+		}
+	})
+	
 	SessionManager.bindToServer(socketServer)
 	socketServer.engine.use(RestService.sessionMiddleware)
 
